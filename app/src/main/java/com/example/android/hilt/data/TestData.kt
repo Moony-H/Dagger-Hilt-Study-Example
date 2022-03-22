cdf4101b01870d52676247061ef6782642cdff4b
Moony-H <moonylight247@gmail.com>
2022-03-23 01:03:52 +0900

    edit readme

--- app/src/main/java/com/example/android/hilt/data/TestData.kt
+++ app/src/main/java/com/example/android/hilt/data/TestData.kt
@@ -2,11 +2,10 @@ package com.example.android.hilt.data
 
 import dagger.hilt.android.scopes.ActivityRetainedScoped
 import dagger.hilt.android.scopes.ActivityScoped
-import dagger.hilt.android.scopes.FragmentScoped
 import javax.inject.Inject
 import javax.inject.Singleton
 
-@FragmentScoped
+@ActivityRetainedScoped
 class TestData @Inject constructor(){
     var data=""
 }
